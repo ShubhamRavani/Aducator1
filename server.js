3765e6c167f240e0a5a30c8c376df948cb543b61
kumarsubhojeet <60726609+kumarsubhojeet@users.noreply.github.com>
2022-04-04 15:39:42 +0530

    first commit

--- server.js
+++ server.js
@@ -31,9 +31,8 @@ app.use("/api/comments", commentRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-
 //server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server is running ${PORT}`));
 
 //3wlFBGEh7XTcFjfm
