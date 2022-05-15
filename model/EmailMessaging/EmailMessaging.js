a084c2e2b59ad498d5f1100d2688020290c26c0a
Shubham Ravani <shubhamravani67890@gmail.com>
2022-05-15 21:38:39 +0530

    Aducator

--- model/EmailMessaging/EmailMessaging.js
+++ model/EmailMessaging/EmailMessaging.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const emailMsgSchema = new mongoose.Schema(
   {
-    fromEmail: {
+    from: {
       type: String,
       required: true,
     },
-    toEmail: {
+    to: {
       type: String,
       required: true,
     },
